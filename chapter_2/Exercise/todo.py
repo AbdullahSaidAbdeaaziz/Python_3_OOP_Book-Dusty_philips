c4e62e3f8dd5d2a7e244b5907881d4a08523ef5d
AbdullahSaidAbdeaaziz <gitHubjoe2@gmail.com>
2025-04-15 11:44:09 +0200

    fix: improve some of code

--- chapter_2/Exercise/todo.py
+++ chapter_2/Exercise/todo.py
@@ -22,7 +22,13 @@ class Task:
 class Todo:
     def __init__(self):
         self.tasks: list[Task] = []
-
+        
+    def _find_task(self, id: int):
+        for task in self.tasks:
+            if id == task.id:
+                return task
+        return None 
+    
     def _check_tasks(self):
         return False if not self.tasks else True
 
@@ -30,34 +36,35 @@ class Todo:
         self.tasks.append(Task(name))
         print("Task has been added.")
 
-    def modify_task(self, id: int, new_name: str):
+    def modify_task(self, id: int, new_name: str) -> bool:
         if self._check_tasks():
             print("There no task added yet!")
-        for task in self.tasks:
-            if task.id == id:
-                task.name = new_name
-                print("Added!")
-                break
+        task = self._find_task(id)
+        if task:
+            task.name = new_name
+            return True
+        return False
+            
 
-    def delete_task(self, id: int):
+    def delete_task(self, id: int) -> bool:
         if self._check_tasks():
             print("There nothing to delete nothing has been added yet!")
 
-        for task in self.tasks:
-            if id == task.id:
-                self.tasks.remove(task)
-                print("Deleted")
-                break
+        task = self._find_task(id)
+        if task:
+            self.tasks.remove(task)
+            return True
         else:
-            print("not found")
+            return False
 
     def mark_task(self, id: int, done: bool = True):
         if self._check_tasks():
             print("There no task to mark it")
 
-        for task in self.tasks:
-            if id == task.id:
-                task.done = done
-                break
+        task = self._find_task(id)
+        if task:
+            task.done = done
+            return True
         else:
             print("Dont have this task")
+            return False
